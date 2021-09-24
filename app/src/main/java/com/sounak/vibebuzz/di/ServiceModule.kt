1ff0907e3295b4592131b6c08943326df08b9ddc
sounak saha <sounaksaha100@gmail.com>
2021-09-24 22:25:00 +0530

    updated music source class

--- app/src/main/java/com/sounak/vibebuzz/di/ServiceModule.kt
+++ app/src/main/java/com/sounak/vibebuzz/di/ServiceModule.kt
@@ -6,6 +6,7 @@ import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.audio.AudioAttributes
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
+import com.sounak.vibebuzz.data.remote.MusicDatabase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -18,6 +19,11 @@ import dagger.hilt.android.scopes.ServiceScoped
 @Module
 @InstallIn(ServiceComponent::class)
 object ServiceModule {
+
+    @ServiceScoped
+    @Provides
+
+    fun provideMusicDatabase () = MusicDatabase()
 
     @ServiceScoped
     @Provides
