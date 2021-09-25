256cf58013e2ac560f36d7f44be93be7360735d0
sounak saha <sounaksaha100@gmail.com>
2021-09-25 13:59:29 +0530

    mainview model updated

--- app/src/main/java/com/sounak/vibebuzz/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/sounak/vibebuzz/ui/viewmodels/MainViewModel.kt
@@ -1,4 +1,86 @@
 package com.sounak.vibebuzz.ui.viewmodels
 
-class MainViewModel {
+
+import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaMetadataCompat.METADATA_KEY_MEDIA_ID
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.sounak.vibebuzz.data.entities.Song
+import com.sounak.vibebuzz.exoplayer.MusicServiceConnection
+import com.sounak.vibebuzz.exoplayer.isEnabled
+import com.sounak.vibebuzz.exoplayer.isPlaying
+import com.sounak.vibebuzz.exoplayer.isPrepared
+import com.sounak.vibebuzz.other.Constants.MEDIA_ROOT_ID
+import com.sounak.vibebuzz.other.Resource
+
+class MainViewModel  @ViewModelInject constructor(
+
+    private val musicServiceConnection: MusicServiceConnection
+) : ViewModel(){
+
+    private val _mediaItems = MutableLiveData<Resource<List<Song>>> ()
+        val mediaItems : LiveData<Resource<List<Song>>> = _mediaItems
+
+    val isConnected = musicServiceConnection.isconnected
+    val networkError = musicServiceConnection.networkError
+    val currPlayingsSong = musicServiceConnection.currplayingSong
+    val playbackState = musicServiceConnection.playbackState
+
+    init {
+        _mediaItems.postValue(Resource.loading(null))
+        musicServiceConnection.subscribe(MEDIA_ROOT_ID,object : MediaBrowserCompat.SubscriptionCallback(){
+            override fun onChildrenLoaded(
+                parentId: String,
+                children: MutableList<MediaBrowserCompat.MediaItem>
+            ) {
+                super.onChildrenLoaded(parentId, children)
+                val items = children.map {
+                    Song(
+                        it.mediaId!!,
+                        it.description.title.toString(),
+                        it.description.subtitle.toString(),
+                        it.description.mediaUri.toString(),
+                        it.description.iconUri.toString()
+                    )
+                }
+                _mediaItems.postValue(Resource.success(items))
+            }
+        })
+    }
+
+    fun skipToNextSong () {
+        musicServiceConnection.transportControls.skipToNext()
+    }
+    fun skipToPreviousSong () {
+        musicServiceConnection.transportControls.skipToPrevious()
+    }
+    fun seekTo(pos: Long) {
+        musicServiceConnection.transportControls.seekTo(pos)
+    }
+
+    fun playOrToggleSong(mediaItem: Song ,toggle : Boolean = false){
+        val isPrepared = playbackState.value?.isPrepared  ?: false
+        if (isPrepared && mediaItem.id ==
+            currPlayingsSong?.value?.getString(METADATA_KEY_MEDIA_ID)) {
+            playbackState.value?.let {playbackState->
+                when{
+                    playbackState.isPlaying -> if(toggle) musicServiceConnection.transportControls.pause()
+                    playbackState.isEnabled -> musicServiceConnection.transportControls.play()
+                    else ->Unit
+
+                }
+
+            }
+        }else{
+            musicServiceConnection.transportControls.playFromMediaId(mediaItem.id ,null)
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        musicServiceConnection.unsubscribe(MEDIA_ROOT_ID, object : MediaBrowserCompat.SubscriptionCallback() {})
+    }
+
 }
